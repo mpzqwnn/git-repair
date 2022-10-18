ba175e90e338f1f7b171fa65108a0b2466b29884
mpzqwnn <854066014@qq.com>
2022-10-18 12:44:51 +0800

    third commit

--- repair/src/components/backstage.vue
+++ repair/src/components/backstage.vue
@@ -1,11 +1,14 @@
 <template>
     <div>
         <el-container>
-            <el-header>后台登录</el-header>
+            <el-header>
+                后台登录
+            </el-header>
             <el-main>
-                <el-input v-model="account" placeholder="请输入账号"></el-input>
-                <el-input v-model="password" placeholder="请输入密码"></el-input>
-                <el-button type="primary">登录</el-button>
+                <div>账号：<el-input v-model="account" placeholder="请输入账号"></el-input></div>
+                <div>密码：<el-input v-model="password" placeholder="请输入密码" show-password></el-input></div>
+                <br>
+                <el-button type="primary">登录</el-button><el-button type="primary">注册</el-button>
             </el-main>
         </el-container>
     </div>
@@ -23,5 +26,19 @@
 </script>
 
 <style lang="scss" scoped>
-
+  .el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+    font-size: 50px;
+  }
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+  }
+  .el-input{
+    width: 20%;
+  }
 </style>
